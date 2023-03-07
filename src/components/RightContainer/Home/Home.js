b211aaee75d870d87609c7482db9fae04a0dafdf
kaigoerg99 <kaigorg@gmail.com>
2023-03-07 15:52:33 -0500

    ui changes

--- src/components/RightContainer/Home/Home.js
+++ src/components/RightContainer/Home/Home.js
@@ -1,7 +1,10 @@
+import React from "react";
+import './Home.css';
+
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="Home">
+            Web developer born in Singapore in 1999. With an eye for design and a passion for visual creation, I build websites with the purpose of making the best user experience possible. I believe that minimalism is the cornerstone of effective and beautiful design. 
         </div>
     )
 }
