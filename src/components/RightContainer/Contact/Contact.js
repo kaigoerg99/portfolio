24632effac94bd85ff2bc7ba9a9bedbeeebe644b
kaigoerg99 <kaigorg@gmail.com>
2023-03-15 16:43:03 -0400

    change links to constants

--- src/components/RightContainer/Contact/Contact.js
+++ src/components/RightContainer/Contact/Contact.js
@@ -3,12 +3,15 @@ import ContactLink from './ContactLink';
 import resume from '../../../img/Resume2023.pdf';
 
 const Contact = () => {
+    const gitHub = 'https://github.com/kaigoerg99';
+    const email = 'mailto: goerg.k@northeastern.edu';
+    const linkedIn = 'https://www.linkedin.com/in/kaigoerg/';
     return (
         <div className="ContactContainer">
-            <ContactLink href='mailto: goerg.k@northeastern.edu' name='Email' icon="fa-solid fa-envelope"/>
+            <ContactLink href={email} name='Email' icon="fa-solid fa-envelope"/>
             <ContactLink href={resume} name='Resume' rel="noreferrer" target="_blank" icon="fa-solid fa-file-lines"/>
-            <ContactLink href='https://github.com/kaigoerg99' name='GitHub' target="_blank" icon="fa-brands fa-square-github"/>
-            <ContactLink href='https://www.linkedin.com/in/kaigoerg/' name='LinkedIn' target="_blank" icon="fa-brands fa-linkedin"/>
+            <ContactLink href={gitHub} name='GitHub' target="_blank" icon="fa-brands fa-square-github"/>
+            <ContactLink href={linkedIn} name='LinkedIn' target="_blank" icon="fa-brands fa-linkedin"/>
         </div>
     )
 }
