dbff16f6f3aa46723be99595b450ec635749229d
kaigoerg99 <kaigorg@gmail.com>
2023-03-15 23:53:58 -0400

    added profile picture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Background from './img/pexels-jimmy-chan-2076189.jpg'
+import background from './img/pexels-jimmy-chan-2076189.jpg'
 import MainContainer from './components/MainContainer/MainContainer.js';
 import { DarkModeProvider } from './context/themeContext';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div
       className="App"
-      style={{backgroundImage: `url(${Background})`}}
+      style={{backgroundImage: `url(${background})`}}
     >
       <DarkModeProvider>
         <BrowserRouter>
--- src/components/RightContainer/Home/Home.js
+++ src/components/RightContainer/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './Home.css';
+import profilePicture from '../../../img/profile.jpg';
 
 const Home = () => {
     return (
-        <div className="Home">
-            Web developer born in Singapore in 1999. With an eye for design and a passion for visual creation, I build websites with the purpose of making the best user experience possible. I believe that minimalism is the cornerstone of effective and beautiful design. 
+        <div className="HomeContainer">
+            <img src={profilePicture} alt='profilePicture' className='ProfilePicture'/>
+            <span className="Text">
+                Web developer born in Singapore in 1999. With an eye for design and a passion for visual creation, I build websites with the purpose of making the best user experience possible. I believe that minimalism is the cornerstone of effective and beautiful design. 
+            </span>
         </div>
     )
 }
