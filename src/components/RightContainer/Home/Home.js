2ca0bdaec86b3e401dc393ab8941ce8cde39a937
kaigoerg99 <kaigorg@gmail.com>
2023-03-17 17:01:48 -0400

    align about text

--- src/components/RightContainer/Home/Home.js
+++ src/components/RightContainer/Home/Home.js
@@ -8,14 +8,14 @@ const Home = () => {
             <img src={profilePicture} alt='profilePicture' className='ProfilePicture'/>
             <div className="TextContainer">
                 <span className="Heading">
-                    A developer & collaborator at heart.
+                    A developer and collaborator at heart.
                 </span>
                 <span className="Text">
                     I am a web developer from Singapore born in 1999.
                     With an eye for design and a passion for visual creation, I build websites and apps with the purpose of making the best user experience possible.
                     I believe that both code and design can be beautiful, and I love efficient and minimalistic design.
                     Group environments are where I thrive, and I am a firm believer in effective communication.
-                    In my free time I work out and watch soccer!
+                    In my free time I lift weights and watch soccer!
                 </span>
             </div>
         </div>
